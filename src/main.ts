5ae266f18b89302f5f993b2ee7044758d280e354
yudhawahyup27 <wyudha104@gmail.com>
2024-09-04 22:48:20 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
 
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
+const pinia = createPinia();
 
-const app = createApp(App)
+app.use(pinia);
+app.use(router);
+app.mount('#app');
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
